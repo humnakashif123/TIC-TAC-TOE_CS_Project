181dba04c7a3e09c91552524f6e9157a28437743
humnakashif123 <167675335+humnakashif123@users.noreply.github.com>
2024-04-21 16:40:35 +0500

    Create interface.cpp

---
+++ interface.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout << endl;
+    cout << "\t\t\t| TIC |\t X  |  O  |" << endl;
+    cout << "\t\t\t-------------------" << endl;
+	cout << "\t\t\t|  X  |\tTAC |  X  |" << endl;
+	cout << "\t\t\t-------------------" << endl;
+	cout << "\t\t\t|  O  |\t X  | TOE |" << endl;
+
+return 0
+}
